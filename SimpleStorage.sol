7d384e761360b8278906ac55bfa700d7677f5021
Arif6474 <arifulislam64743@gmail.com>
2025-02-19 00:57:40 +0600

    contract simpleStorage

---
+++ SimpleStorage.sol
@@ -0,0 +1,12 @@
+
+ // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract SimpleStorage{
+    bool hasFavoriteNumber = true;
+    uint256 favoriteNumber = 100;
+    int256 favoriteInt = -100;
+    string favoriteNumberToString = "eighty-eight";
+    address myAddress = 0xdD2FD4581271e230360230F9337D5c0430Bf44C0;
+    bytes32 favoriteByte32 = "cat";
+}
